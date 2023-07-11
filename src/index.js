3f9a9a849adee4a8c87d63ed94ec57cedb40f397
Giraltdereve <celine.rouaire@gmail.com>
2023-07-11 18:19:53 +0200

    retest routes

--- src/index.js
+++ src/index.js
@@ -29,6 +29,33 @@
 //   // deuxiéme argument de la fonction : l'élément DOM cible
 // );
 
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+// import Header from './components/Header.jsx';
+// import Home from './Pages/Home.jsx';
+// import Project from './Pages/Project.jsx';
+// import About from './Pages/About.jsx';
+// import Contacts from './components/Contacts.jsx';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router basename="/Portfolio-2023/">
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/Project/:id/" element={<Project />} />
+//         <Route path="/About/" element={<About />} />
+//         {/* <Route path="*" element={<Error />} /> */}
+//       </Routes>
+//       <Contacts />
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -42,7 +69,7 @@ import Contacts from './components/Contacts.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/Portfolio-2023/">
+    <Router basename="/Portfolio-2023">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
